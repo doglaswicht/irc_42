932d1d265f9f3e3e3c889369f13c7295d77186ce
dw <doglasbastos@hotmail.com>
2026-09-01 15:17:45 +0200

    corrections comantaires

--- classes/Message.hpp
+++ classes/Message.hpp
@@ -13,20 +13,10 @@ class Message
         Message();
 
         Message(std::string message);
-
-  
         void    add_to_message(std::string message);
-  
-        //-------------------------------------------------
-        //setter
         void    set_message(std::string message);
-        //-------------------------------------------------
-        
-
-        //-------------------------------------------------
-        //guetteur
         std::string get_message();
-        //----------------------------------------------------
+
 
 };
 
