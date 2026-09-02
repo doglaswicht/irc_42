01879db155cbfc4102025361f506ada7e5db0a96
doglaswicht <doglasbastos@hotmail.com>
2026-09-02 19:57:08 +0200

    remove web/add fedor

--- classes/Message.cpp
+++ classes/Message.cpp
@@ -17,19 +17,13 @@
             this->_message = this->_message + message;
         }
   
-        //-------------------------------------------------
-        //setter
         void    Message::set_message(std::string message)
         {
             this->_message = message;
         }
-        //-------------------------------------------------
-        
 
-        //-------------------------------------------------
-        //guetteur
         std::string Message::get_message()
         {
             return (this->_message);
         }
-        //----------------------------------------------------
+    
